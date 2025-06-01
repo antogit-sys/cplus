3f59a314aca4826e4cc6f8631ab572246c97ba65
antogit-sys <antonioperrgiorgi@gmail.com>
2025-06-01 17:22:23 +0200

    sector cplus

---
+++ include/cplus.h
@@ -0,0 +1,11 @@
+#ifndef CPLUS_H
+#   define CPLUS_H
+
+#   include "utils/cplus_utils.h"   // Macro generali e costruttori
+#   include "memory/cplus_memory.h" // Gestione memoria
+#   include "class/cplus_class.h"   // Definizioni classi
+#   include "vtable/cplus_vtable.h" // Gestione metodi virtuali
+                                    
+#endif // CPLUS_H
+
+
