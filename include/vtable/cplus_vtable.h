6390c80f24ed84ce7e351685a8870e60aacc9a5f
antogit-sys <antonioperrgiorgi@gmail.com>
2025-06-01 17:37:55 +0200

    add cplus_vtable header

---
+++ include/vtable/cplus_vtable.h
@@ -0,0 +1,22 @@
+#ifndef CPLUS_VTABLE_H
+#define CPLUS_VTABLE_H
+
+// Macro per definire un metodo virtuale nella vtable
+// Definisce un puntatore a funzione con il nome del metodo, tipo di ritorno, tipo del parametro 'self' e altri parametri opzionali
+// Esempio: METHOD(void, print, Point*) diventa: void (*print)(Point* self)
+#define METHOD(return_type, func_name, self_type, ...) \
+    return_type (*func_name)(self_type self, ##__VA_ARGS__)
+
+// Macro per dichiarare la struttura della vtable di una classe
+// Espande in: struct NomeClasseVTable
+// Dove verranno definiti i puntatori alle funzioni/metodi virtuali della classe
+#define DECLARE_VTABLE(classname) \
+    struct classname##VTable
+
+// Macro per definire e inizializzare la vtable di una classe con i metodi virtuali forniti
+// Espande in: const NomeClasseVTable NomeClasse_vtable = { metodi... }
+// Esempio: ADD_METHODS(Point, Point_print, Point_delete)
+#define ADD_METHODS(classname, ...) \
+    const classname##VTable classname##_vtable = { __VA_ARGS__ }
+
+#endif // CPLUS_VTABLE_H
